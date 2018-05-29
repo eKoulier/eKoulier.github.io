1ca551ddb34be07cb55d266c1115fb29a853b703
Eleftherios Koulierakis <elkoulierakis.n@gmail.com>
2018-05-30 00:30:33 +0200

    Checked another template

--- gulpfile.js
+++ gulpfile.js
@@ -44,12 +44,6 @@ gulp.task('vendor', function() {
     ])
     .pipe(gulp.dest('./vendor/jquery'))
 
-  // jQuery Easing
-  gulp.src([
-      './node_modules/jquery.easing/*.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery-easing'))
-
 });
 
 // Compile SCSS
